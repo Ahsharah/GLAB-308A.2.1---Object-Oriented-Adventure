25108d4888616a79b1555b74f188bb7550b7f400
Alexandria <walkerciara24@gmail.com>
2024-10-15 10:25:00 -0500

    Update GLAB 308A.2.1: Object-Oriented.js

--- GLAB 308A.2.1: Object-Oriented.js
+++ GLAB 308A.2.1: Object-Oriented.js
@@ -37,8 +37,23 @@ const adventurer = {
         inventory: ["small hat", "sunglasses"]
       }
     },
+    // Adding roll method to adventurer. 
+    //The (mod = 0) part is the parameter list for this function.
     roll(mod = 0) {
       const result = Math.floor(Math.random() * 20) + 1 + mod;
       console.log(`${this.name} rolled a ${result}.`);
     }
   };
+
+  // Create Character class.
+  class Character {
+    constructor(name) {
+        this.name = name;
+        this.health = 100;
+        this.inventory = [];
+    }
+roll(mod = 0) {
+    const result = Math.floor(Math.random() * 20) + 1 + mod;
+    console.log('${this.name} rolled a ${result}.');
+}
+  }
