ee3ab98ba1964c4188c573008c76594df1435937
Alexandria <walkerciara24@gmail.com>
2024-10-15 10:35:29 -0500

    Update GLAB 308A.2.1: Object-Oriented.js

--- GLAB 308A.2.1: Object-Oriented.js
+++ GLAB 308A.2.1: Object-Oriented.js
@@ -1,5 +1,5 @@
 // Create the adventurer object.
-const adventurer = {
+/*const adventurer = {
     name: "Robin",
     health: 10,
     inventory: ["sword", "potion", "artifact"]
@@ -11,18 +11,15 @@ for (let item of adventurer.inventory) {
 }
 
 // Add "Leo" to the adventurer..!
-const adventurer = {
+/*const adventurer = {
     name: "Robin",
     health: 10,
     inventory: ["sword", "potion", "artifact"],
     companion: {
         name:"Leo",
         type: "Cat"
-
-
     }
-};
-
+}; 
 // Adding Frank, Leo's companion:
 const adventurer = {
     name: "Robin",
@@ -44,7 +41,6 @@ const adventurer = {
       console.log(`${this.name} rolled a ${result}.`);
     }
   };
-
   // Create Character class.
   class Character {
     constructor(name) {
@@ -57,3 +53,48 @@ roll(mod = 0) {
     console.log('${this.name} rolled a ${result}.');
 }
   }
+  // Recreate Robin using the Character class.
+const robin = new Character ('Robin');
+robin.inventory = ["sword", "potion", "artifact"];
+robin. companion = new Character("Leo");
+robin.companion.type = "Cat";
+robin.companion.companion = new Character("Frank");
+robin.companion.companion = "Flea";
+robin.companion.companion.inventory = ["small hat", "sunnies"];
+*/
+
+// Create the Character class
+class Character {
+    constructor(name) {
+        this.name = name;
+        this.health = 100;
+        this.inventory = [];
+    }
+
+    roll(mod = 0) {
+        const result = Math.floor(Math.random() * 20) + 1 + mod;
+        console.log(`${this.name} rolled a ${result}.`);
+        return result;
+    }
+}
+
+// Create Robin using the Character class
+const robin = new Character('Robin');
+robin.inventory = ["sword", "potion", "artifact"];
+robin.companion = new Character("Leo");
+robin.companion.type = "Cat";
+robin.companion.companion = new Character("Frank");
+robin.companion.companion.type = "Flea";
+robin.companion.companion.inventory = ["small hat", "sunglasses"];
+
+// Demonstrate functionality
+console.log("Robin's inventory:");
+for (let item of robin.inventory) {
+    console.log(item);
+}
+
+robin.roll();
+robin.companion.roll();
+robin.companion.companion.roll();
+
+console.log(robin);
