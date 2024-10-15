5b0c6855578abbf59b318c870502564f32024486
Alexandria <walkerciara24@gmail.com>
2024-10-15 09:33:38 -0500

    Updates

--- GLAB 308A.2.1: Object-Oriented.js
+++ GLAB 308A.2.1: Object-Oriented.js
@@ -1,6 +1,34 @@
+// Create the adventurer object.
 const adventurer = {
     name: "Robin",
     health: 10,
     inventory: ["sword", "potion", "artifact"]
     }
-    
+   
+    // Create a loop to log each item in Robin's inventory.
+for (let item of adventurer.inventory) {
+    console.log(item);
+}
+
+// Add "Leo" to the adventurer..!
+const adventurer = {
+    name: "Robin",
+    health: 10,
+    inventory: ["sword", "potion", "artifact"],
+    companion: {
+        name:"Leo",
+        type: "Cat"
+
+
+    }
+};
+
+// Adding Frank, Leo's companion:
+const adventurer = {
+    name: "Robin",
+    health: 10,
+    inventory: ['sword', 'potion', 'artifact'],
+    companion: {
+        name: "Leo"
+    }
+}
